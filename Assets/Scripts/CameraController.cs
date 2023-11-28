6652f4ce4bd14277ada0bdd0ee895fdb1a982f6c
mdl22 <mdl22@cam.ac.uk>
2023-11-28 16:48:02 +0000

    slider control panel implemented

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] float speed = 10;
     [SerializeField] float turnSpeed = 40;
+    [SerializeField] float cutoff = 0.1f;
+    float zoomValue;
+    float spinValue;
+
+    [SerializeField] Slider zoomSlider;
+    [SerializeField] Slider spinSlider;
 
     void Update()
     {
+        zoomValue = FindObjectOfType<PlayerController>().RecalibrateSlider(zoomSlider, cutoff);
+        spinValue = FindObjectOfType<PlayerController>().RecalibrateSlider(spinSlider, cutoff);
+
+        // Zoomd in and out based on key input and slider
         if (Input.GetKey(KeyCode.F))
         {
             transform.position += Vector3.forward * speed * Time.deltaTime;
@@ -17,6 +28,9 @@ public class CameraController : MonoBehaviour
         {
             transform.position += Vector3.back * speed * Time.deltaTime;
         }
+        transform.position += Vector3.forward * speed * Time.deltaTime * zoomValue;
+
+        // Rotates around y-axis based on key input and slider
         if (Input.GetKey(KeyCode.L))
         {
             transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
@@ -25,9 +39,6 @@ public class CameraController : MonoBehaviour
         {
             transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.Alpha0))
-        {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-        }
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * spinValue);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,36 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.UI;
-using TMPro;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float turnSpeed = 40;
-    float horizontalInput;
-    float verticalInput;
+    [SerializeField] float cutoff = 0.1f;
+    float horizontalKeyValue;
+    float verticalKeyValue;
+    float horizontalSliderValue;
+    float verticalSliderValue;
 
     [SerializeField] Transform customPivot;
-    //[SerializeField] Slider horizontalSlider;
-    //[SerializeField] Slider verticalSlider;
+    [SerializeField] Slider horizontalSlider;
+    [SerializeField] Slider verticalSlider;
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        horizontalKeyValue = -Input.GetAxis("Horizontal");
+        verticalKeyValue = Input.GetAxis("Vertical");
 
-        // Rotates around y-axis based on horizontal input
+        horizontalSliderValue = -RecalibrateSlider(horizontalSlider, cutoff);
+        verticalSliderValue = -RecalibrateSlider(verticalSlider, cutoff);
+
+        // Rotates around y-axis based on horizontal input and slider
         transform.RotateAround(customPivot.position, Vector3.up,
-            Time.deltaTime * turnSpeed * horizontalInput);
+            turnSpeed * Time.deltaTime * (horizontalKeyValue + horizontalSliderValue));
 
-        // Rotates around x-axis based on vertical input
+        // Rotates around x-axis based on vertical input and slider
         transform.RotateAround(customPivot.position, Vector3.right,
-            Time.deltaTime * turnSpeed * verticalInput);
+            turnSpeed * Time.deltaTime * (verticalKeyValue + verticalSliderValue));
+    }
 
-        // Rotates around y-axis based on horizontal input
-        //transform.RotateAround(customPivot.position, Vector3.up, horizontalSlider.value);
-
-        // Rotates around x-axis based on vertical input
-        //transform.RotateAround(customPivot.position, Vector3.right, verticalSlider.value);
+    public float RecalibrateSlider(Slider slider, float cutoff)
+    // Returns 0 at -cutoff and cutoff, but maxValue and minValue unchanged
+    {
+        return Math.Abs(slider.value) > cutoff ?
+            (slider.value - cutoff) / (slider.maxValue - cutoff) : 0;
     }
 }
