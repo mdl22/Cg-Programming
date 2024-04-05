d3e581eb007a0b070a94108119f7030411ffafca
mdl22 <mdl22@cam.ac.uk>
2024-04-05 18:02:26 +0100

    emission map functionality

--- Assets/Scripts/PanelControl/ClickOnRegion.cs
+++ Assets/Scripts/PanelControl/ClickOnRegion.cs
@@ -14,16 +14,21 @@ public class ClickOnRegion : MonoBehaviour
     [SerializeField] TextMeshProUGUI areaTitleText;
     [SerializeField] TextMeshProUGUI areaDescriptionText;
 
+    public Material material;
     Texture2D texture;
     [SerializeField] Texture2D mask;
     [SerializeField] Texture2D[] emissionMaps;
     [SerializeField] TextAsset maskTable;
 
     Dictionary<int, string[]> areas = new Dictionary<int, string[]>();
+    Dictionary<int, Texture2D> maps = new Dictionary<int, Texture2D>();
 
     void Start()
     {
-        texture = GetComponent<Renderer>().material.mainTexture as Texture2D;
+        material = GetComponent<Renderer>().material;
+        material.EnableKeyword("_EMISSION");
+
+        texture = material.mainTexture as Texture2D;
 
         panelListText.text = "";
         foreach (string line in maskTable.text.Split("\n"))
@@ -31,7 +36,10 @@ public class ClickOnRegion : MonoBehaviour
             string[] fields = line.Split("\t");
             if (line.Length > 0 && Char.IsDigit(fields[0][0]))
             {
-                areas.Add(1 << Convert.ToInt32(fields[0]), new string[] {fields[2], fields[3]});
+                int rChannel = 1 << Convert.ToInt32(fields[0]);
+                areas.Add(rChannel, new string[] {fields[2], fields[3]});
+                maps.Add(rChannel, emissionMaps[maps.Count]);
+
                 panelListText.text = string.Concat(
                     string.Concat(panelListText.text, fields[2]), "\n\n");
             }
@@ -58,14 +66,16 @@ public class ClickOnRegion : MonoBehaviour
                 int areasKey = 1 << pixelColor.r;
                 if (areas.ContainsKey(areasKey))
                 {
+                    material.SetTexture("_EmissionMap", emissionMaps[0]);
+
                     areaTitleText.text = areas[areasKey][0];
                     areaDescriptionText.text = areas[areasKey][1];
 
-                    other.GetComponent<Reset>().ResetPanelText(false);
+                    other.GetComponent<Reset>().ResetPanel(false);
                 }
                 else
                 {
-                    other.GetComponent<Reset>().ResetPanelText();
+                    other.GetComponent<Reset>().ResetPanel();
                 }
             }
         }
--- Assets/Scripts/PanelControl/Reset.cs
+++ Assets/Scripts/PanelControl/Reset.cs
@@ -11,26 +11,38 @@ public class Reset : MonoBehaviour
     [SerializeField] TextMeshProUGUI panelListText;
     [SerializeField] TextMeshProUGUI areaTitleText;
     [SerializeField] TextMeshProUGUI areaDescriptionText;
-    [SerializeField] Button resetButton;
     [SerializeField] Button areasButton;
     [SerializeField] Button backButton;
 
-    public void ResetPanelText(bool boolean = true)
+    public void ResetPanel(bool reset = true)
     {
-        panelTitleText.gameObject.SetActive(boolean);
-        panelListText.gameObject.SetActive(boolean);
+        // get material from ClickOnRegion script attached child (default gameobject)
+        // of first and only active child of GameObject
+        Material material = GetComponentInChildren<ClickOnRegion>().material;
 
-        areaTitleText.gameObject.SetActive(!boolean);
-        areaDescriptionText.gameObject.SetActive(!boolean);
+        if (reset)
+        {
+            material.SetColor("_EmissionColor", new Color32(0, 0, 0, 0));       // Black
+        }
+        else
+        {
+            material.SetColor("_EmissionColor", new Color32(127, 159, 187, 0)); // Blue
+        }
 
-        backButton.gameObject.SetActive(!boolean); 
+        panelTitleText.gameObject.SetActive(reset);
+        panelListText.gameObject.SetActive(reset);
+
+        areaTitleText.gameObject.SetActive(!reset);
+        areaDescriptionText.gameObject.SetActive(!reset);
+
+        backButton.gameObject.SetActive(!reset); 
     }
 
-    public void ResetPanel()
+    public void ClosePanel()
     {
         areasPanel.gameObject.SetActive(false); 
         areasButton.gameObject.SetActive(true); 
 
-        ResetPanelText();
+        ResetPanel();
     }
 }
